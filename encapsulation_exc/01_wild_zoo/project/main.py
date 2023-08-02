33c72edd83f5421d56f9365afe1d84d70f0c291f
zhmarinov <j.marinovmt@gmail.com>
2023-08-03 00:38:16 +0300

    updated zoo.py from 01_wild_zoo

--- encapsulation_exc/01_wild_zoo/project/main.py
+++ encapsulation_exc/01_wild_zoo/project/main.py
@@ -23,13 +23,13 @@ for i in range(len(animals)):
  print(zoo.add_animal(animal, price))
 # Adding all workers
 for worker in workers:
- print(zoo.hire_worker(worker))
- # Tending animals
- print(zoo.tend_animals())
- # Paying keepers
- print(zoo.pay_workers())
- # Fireing worker
- print(zoo.fire_worker("Adam"))
- # Printing statuses
- print(zoo.animals_status())
- print(zoo.workers_status())
+    print(zoo.hire_worker(worker))
+    # Tending animals
+print(zoo.tend_animals())
+# Paying keepers
+print(zoo.pay_workers())
+# Fireing worker
+print(zoo.fire_worker("Adam"))
+# Printing statuses
+print(zoo.animals_status())
+print(zoo.workers_status())
--- encapsulation_exc/01_wild_zoo/project/zoo.py
+++ encapsulation_exc/01_wild_zoo/project/zoo.py
@@ -31,6 +31,7 @@ class Zoo:
         if self.__workers_capacity > len(self.workers):
             self.workers.append(worker)
             return f"{worker.name} the {worker.__class__.__name__} hired successfully"
+
         return "Not enough space for worker"
 
     def fire_worker(self, worker_name):
@@ -47,6 +48,7 @@ class Zoo:
         if sum_of_salaries <= self.__budget:
             self.__budget -= sum_of_salaries
             return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+
         return "You have no budget to pay your workers. They are unhappy"
 
     def tend_animals(self):
@@ -61,70 +63,43 @@ class Zoo:
         self.__budget += amount
 
     def animals_status(self):
-        result = f"You have {len(self.animals)} animals"
-        lions = [a for a in self.animals if a.__class__.__name__ == "Lions"]
+        result = f"You have {len(self.animals)} animals\n"
+
+        lions = [a for a in self.animals if a.__class__.__name__ == "Lion"]
         tigers = [a for a in self.animals if a.__class__.__name__ == "Tiger"]
         cheetahs = [a for a in self.animals if a.__class__.__name__ == "Cheetah"]
 
-        result += f"----- {len(lions)} Lions: \n"
+        result += f"----- {len(lions)} Lions:\n"
         for l in lions:
             result += f"{l}\n"
 
-        result += f"----- {len(tigers)} Tigers: \n"
+        result += f"----- {len(tigers)} Tigers:\n"
         for t in tigers:
             result += f"{t}\n"
 
-        result += f"----- {len(cheetahs)} Cheetahs: \n"
+        result += f"----- {len(cheetahs)} Cheetahs:\n"
         for c in cheetahs:
             result += f"{c}\n"
+
         return result[:-1]
 
     def workers_status(self):
-        result = result = f"You have {len(self.workers)} animals"
-        keepers = [w for w in self.workers if w.__class__.__name__ == "Keeper"]
-        caretakers = [w for w in self.workers if w.__class__.__name__ == "Caretakers"]
-        vets = [w for w in self.workers if w.__class__.__name__ == "Vets"]
+        result = result = f"You have {len(self.workers)} workers\n"
 
-        result += f"----- {len(keepers)} Keepers: \n"
+        keepers = [w for w in self.workers if w.__class__.__name__ == "Keeper"]
+        caretakers = [w for w in self.workers if w.__class__.__name__ == "Caretaker"]
+        vets = [w for w in self.workers if w.__class__.__name__ == "Vet"]
+
+        result += f"----- {len(keepers)} Keepers:\n"
         for k in keepers:
             result += f"{k}\n"
 
-            result += f"----- {len(caretakers)} Caretakers: \n"
+        result += f"----- {len(caretakers)} Caretakers:\n"
         for c in caretakers:
             result += f"{c}\n"
 
-            result += f"----- {len(vets)} Vets: \n"
+        result += f"----- {len(vets)} Vets:\n"
         for v in vets:
             result += f"{v}\n"
 
         return result[:-1]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
